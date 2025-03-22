d642364c8cecfd5b859764a7a5e02f0bff5f99e1
Rafin298 <rafinkhan298@gmail.com>
2025-03-22 16:45:20 +0600

    updated host

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS.extend(filter(None, os.environ.get('ALLOWED_HOSTS', '').split(',')))
+# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS.extend(filter(None, os.environ.get('ALLOWED_HOSTS', '').split(',')))
+ALLOWED_HOSTS = ['localhost', 'ec2-3-26-178-91.ap-southeast-2.compute.amazonaws.com']
 
 
 # Application definition
